724f19f6dff15001e0d9edeaaa382c032f1bbf1a
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2023-08-18 15:47:03 -0400

    first commit

---
+++ src/main/java/inicio/guirmes/demo/model/ParkingSpotModel.java
@@ -0,0 +1,14 @@
+package inicio.guirmes.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "TB_PARKING_SPOT")
+public class ParkingSpotModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+
+}
